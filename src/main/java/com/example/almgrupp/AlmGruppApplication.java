2a29555a8e8756c434113cf21d92e406d1942464
Sofya Dziamidava <sofya.dziamidava@gmail.com>
2022-09-02 13:59:53 +0200

    Added some tests to Controller class

--- src/main/java/com/example/almgrupp/AlmGruppApplication.java
+++ src/main/java/com/example/almgrupp/AlmGruppApplication.java
@@ -1,5 +1,6 @@
 package com.example.almgrupp;
 
+import com.example.almgrupp.controller.Controller;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
